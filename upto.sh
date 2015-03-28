dbd53eec694d89b4f70a97d43dd1a038cd475d7b
driv <federiconafria@gmail.com>
2015-03-28 14:12:20 -0300

    Removed external conditional, transformed into a unique function that has to be registered as such

--- upto.sh
+++ upto.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 function _upto() {
-	CURRENT_FOLDER="$(pwd)"
-	MATCHED_DIR=""
-	MATCHING=true
-
-	while [ "$MATCHING" = true ]; do
-		if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
-			MATCHED_DIR="$CURRENT_FOLDER"
-			CURRENT_FOLDER="$(dirname $CURRENT_FOLDER)"
-		else
-			MATCHING=false
-		fi
-	done
-	if [ -n "$MATCHED_DIR" ]; then
-		cd $MATCHED_DIR
+	EXPRESSION="$1"
+	if [ -z "$EXPRESSION" ]; then
+		echo "A folder expression must be provided"
 	else
-		echo "No Match."
+		CURRENT_FOLDER="$(pwd)"
+		MATCHED_DIR=""
+		MATCHING=true
+
+		while [ "$MATCHING" = true ]; do
+			if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
+				MATCHED_DIR="$CURRENT_FOLDER"
+				CURRENT_FOLDER="$(dirname $CURRENT_FOLDER)"
+			else
+				MATCHING=false
+			fi
+		done
+		if [ -n "$MATCHED_DIR" ]; then
+			cd $MATCHED_DIR
+		else
+			echo "No Match."
+		fi
 	fi
 }
 
-EXPRESSION="$1"
-if [ -z "$EXPRESSION" ]; then
-	echo "A folder expression must be provided"
-else
- _upto
-fi
