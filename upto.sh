94bf389d8fee74c554cb1f50c7ea0f1dbc788b59
driv <federiconafria@gmail.com>
2016-05-03 22:56:40 +0200

    -Adding error returns -Removing now unnecessary else -Redirecting error messages to stderr
    This fixes #1

--- upto.sh
+++ upto.sh
@@ -2,25 +2,26 @@
 function upto() {
 	EXPRESSION="$1"
 	if [ -z "$EXPRESSION" ]; then
-		echo "A folder expression must be provided"
-	else
-		CURRENT_FOLDER="$(pwd)"
-		MATCHED_DIR=""
-		MATCHING=true
-
-		while [ "$MATCHING" = true ]; do
-			if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
-				MATCHED_DIR="$CURRENT_FOLDER"
-				CURRENT_FOLDER="$(dirname $CURRENT_FOLDER)"
-			else
-				MATCHING=false
-			fi
-		done
-		if [ -n "$MATCHED_DIR" ]; then
-			cd $MATCHED_DIR
-		else
-			echo "No Match."
-		fi
+		echo "A folder expression must be provided." >&2
+		return 1
 	fi
-}
+	CURRENT_FOLDER="$(pwd)"
+	MATCHED_DIR=""
+	MATCHING=true
 
+	while [ "$MATCHING" = true ]; do
+		if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
+			MATCHED_DIR="$CURRENT_FOLDER"
+			CURRENT_FOLDER="$(dirname $CURRENT_FOLDER)"
+		else
+			MATCHING=false
+		fi
+	done
+	if [ -n "$MATCHED_DIR" ]; then
+		cd $MATCHED_DIR
+	else
+		echo "No Match." >&2
+		return 1
+	fi
+	
+}
