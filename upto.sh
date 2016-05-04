3a19e1c4ccf2397f153d37cd3528a25e03f18039
driv <federiconafria@gmail.com>
2016-05-04 09:35:57 +0200

    Multiple small fixes - using local variables - adding special case for `\` (Fixes #3) - adding quotings to commands

--- upto.sh
+++ upto.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 function upto() {
-	EXPRESSION="$1"
+	local EXPRESSION="$1"
 	if [ -z "$EXPRESSION" ]; then
 		echo "A folder expression must be provided." >&2
 		return 1
 	fi
-	CURRENT_FOLDER="$(pwd)"
-	MATCHED_DIR=""
-	MATCHING=true
+	if [ "$EXPRESSION" = "/" ]; then
+		cd "/"
+		return 0
+	fi
+	local CURRENT_FOLDER="$(pwd)"
+	local MATCHED_DIR=""
+	local MATCHING=true
 
 	while [ "$MATCHING" = true ]; do
 		if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
 			MATCHED_DIR="$CURRENT_FOLDER"
-			CURRENT_FOLDER="$(dirname $CURRENT_FOLDER)"
+			CURRENT_FOLDER=$(dirname "$CURRENT_FOLDER")
 		else
 			MATCHING=false
 		fi
 	done
 	if [ -n "$MATCHED_DIR" ]; then
-		cd $MATCHED_DIR
+		cd "$MATCHED_DIR"
+		return 0
 	else
 		echo "No Match." >&2
 		return 1
 	fi
-	
 }
