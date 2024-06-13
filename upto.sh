7bd4fe4b763bb418779c2a927759b277a2c0579c
Federico Nafria <federiconafria@gmail.com>
2024-06-13 08:35:37 +0200

    Use PATH instead of FOLDER

--- upto.sh
+++ upto.sh
@@ -2,21 +2,21 @@
 function upto() {
 	local EXPRESSION="$1"
 	if [ -z "$EXPRESSION" ]; then
-		echo "A folder expression must be provided." >&2
+		echo "A path expression must be provided." >&2
 		return 1
 	fi
 	if [ "$EXPRESSION" = "/" ]; then
 		cd "/"
 		return 0
 	fi
-	local CURRENT_FOLDER="$(pwd)"
+	local CURRENT_PATH="$(pwd)"
 	local MATCHED_DIR=""
 	local MATCHING=true
 
 	while [ "$MATCHING" = true ]; do
-		if [[ "$CURRENT_FOLDER" =~ "$EXPRESSION" ]]; then
-			MATCHED_DIR="$CURRENT_FOLDER"
-			CURRENT_FOLDER=$(dirname "$CURRENT_FOLDER")
+		if [[ "$CURRENT_PATH" =~ "$EXPRESSION" ]]; then
+			MATCHED_DIR="$CURRENT_PATH"
+			CURRENT_PATH=$(dirname "$CURRENT_PATH")
 		else
 			MATCHING=false
 		fi
